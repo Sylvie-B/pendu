182d224834d6ba8e235a5ca060e8024279e77443
Sylvie-B <unsolo@hotmail.fr>
2020-12-17 11:34:22 +0100

    input letter

--- appPendu.js
+++ appPendu.js
@@ -10,9 +10,14 @@ let word = [
 // random on table
 let w = Math.ceil(Math.random() * word.length-1);
 let result = word[w];
+console.log(result);
+
+// display hollow word
+
 
 // get user letter
-
+let letter = document.getElementById('letter');
+letter.focus();
 
 // is the letter in the word ?   ! is it more than one ?
 
