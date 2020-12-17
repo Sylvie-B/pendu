d93445bbad667ede6591bca44c820909276f6f05
Sylvie-B <unsolo@hotmail.fr>
2020-12-17 11:25:26 +0100

    5 words, random word

---
+++ appPendu.js
@@ -0,0 +1,27 @@
+// table of words
+let word = [
+    'elephant',
+    'maison',
+    'devinette',
+    'voiture',
+    'ordinateur'
+]
+
+// random on table
+let w = Math.ceil(Math.random() * word.length-1);
+let result = word[w];
+
+// get user letter
+
+
+// is the letter in the word ?   ! is it more than one ?
+
+// display right letter(s)
+
+// say user if not
+
+// win : say user find the good word & random on table
+
+// loose : display word & say game over     ! count test
+
+// bonus : display pendu
