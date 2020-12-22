f357d69f6cc004e03011120381bf9cb33effa1ac
Sylvie-B <unsolo@hotmail.fr>
2020-12-22 14:37:32 +0100

    test object

--- appPendu.js
+++ appPendu.js
@@ -1,15 +1,48 @@
-// table of words
-let allWord = [
-    'ELEPHANT',
-    'MAISON',
-    'DEVINETTE',
-    'VOITURE',
-    'ORDINATEUR',
-    'FORMATION',
-    'DISPARITION'
-]
 
-let chance = 8;
+// table of words
+let allWord = ['ELEPHANT', 'MAISON', 'DEVINETTE', 'VOITURE', 'ORDINATEUR', 'FORMATION', 'DISPARITION']
+
+// object word
+let wordToFind = function (noun) {
+
+    this.word = noun;
+
+    this.getLetter = function () {
+        let allLetter = Array.from(this.word);
+        for (let i = 0; i < allLetter.length; i++) {
+            // display hollow letters
+            let letterCase = document.createElement('div');
+            letterCase.innerHTML = allLetter[i].toString();
+            hollow.appendChild(letterCase);
+            console.log(letterCase);
+        }
+        return allLetter;
+    }
+
+    this.setLetter = function () {
+        // this.word = this.word.toUpperCase();
+        // find & replace é, è, ê, î
+        this.word = this.word.replaceAll('é', 'e');
+        this.word = this.word.replaceAll('è', 'e');
+        this.word = this.word.replaceAll('ê', 'e');
+        this.word = this.word.replaceAll('î', 'i');
+        this.word = this.word.replaceAll('â', 'a');
+        this.word = this.word.replaceAll('ô', 'o');
+        this.word = this.word.toUpperCase();
+    }
+}
+
+// new random word
+let w = Math.ceil(Math.random() * allWord.length-1);
+let theWord = new wordToFind(allWord[w]);
+
+// hollow display
+let hollow = document.getElementById('hollow');
+// display hollow letters
+theWord.getLetter();
+
+// number of chance
+let chance = 5;
 document.getElementById('nbr').innerHTML = chance.toString();
 
 // get infos
@@ -17,77 +50,72 @@ let wrong = document.getElementById('wrongLetter');
 let answer = document.getElementById('answer');
 let nbr = document.getElementById('nbr');
 
-// random on table = 1 of allWord
-let w = Math.ceil(Math.random() * allWord.length-1);
-
-// letter to table & hollow display
-let hollow = document.getElementById('hollow');
-
-for (let i = 0 ; i < allWord[w].length; i++){
-    // display hollow letters
-    let eachLetter = document.createElement('div');
-    eachLetter.innerHTML = allWord[w][i];
-    eachLetter.style.backgroundColor = 'gray';
-    eachLetter.style.width = '3vw';
-    eachLetter.style.margin = '5px';
-    hollow.appendChild(eachLetter);
-    console.log(allWord[w][i]);
-}
-
-// get user letter & word
+// get form letter & form word
 let inputLetter = document.getElementById('letter');
 inputLetter.focus();
 let inputWord = document.getElementById('word');
 
-//  submit letter
+//  submit button letter
 let submit = document.getElementById('submit');
 submit.addEventListener('click', function (){
     let letter = inputLetter.value.toUpperCase();
     inputLetter.value = "";
     inputLetter.focus();
-    console.log(letter);
     testLetter(letter);
-})
-
-// submit word
-let submitWord = document.getElementById('submitWord');
-submitWord.addEventListener('click', function (){
-    // account
-    chance --;
-    document.getElementById('nbr').innerHTML = chance.toString();
-    // test word
-    let test = inputWord.value.toUpperCase();
-    if(allWord[w] === test){
-        console.log("you win");
-    }
-    else{
-        console.log("you loose")
-    }
-    inputWord.value = "";
 })
 
 // function testLetter
 function testLetter (letter){
-    // is the letter in the word ?
-    if(allWord[w].includes(letter)){
-        answer.innerHTML = "la lettre se trouve dans le mot";
-        // if true, where ?
-        let spell = document.getElementById('hollow').getElementsByTagName('div');
-        // console.log("spell = " + spell[0].innerHTML);
-        for (let s = 0; s < allWord[w].length; s++){
-            if (letter === allWord[w][s]){
-                spell[s].style.color = 'black';
+    if (chance > 0){
+        // is the letter in the word ?
+        if(theWord.word.includes(letter)){
+            answer.innerHTML = "la lettre se trouve dans le mot";
+            // if true, where ?
+            let spell = document.getElementById('hollow').getElementsByTagName('div');
+            // console.log("spell = " + spell[0].innerHTML);
+            for (let s = 0; s < theWord.word.length; s++){
+                if (letter === theWord.word[s]){
+                    spell[s].style.color = 'black';
+                }
             }
         }
+        else{   // say user if not
+            answer.innerHTML = "la lettre ne se trouve pas dans le mot";
+            chance --;
+            nbr.innerHTML = chance.toString();
+            wrong.innerHTML += letter;
+        }
     }
-    else{   // say user if not
-        answer.innerHTML = "la lettre ne se trouve pas dans le mot";
+    else {
+        // loose : display word & say game over     ! count test
+        answer.innerHTML = "Il n'y a plus de chance, vous avez perdu";
     }
 }
 
-// win : say user find the good word & random on table
+// submit word
+let submitWord = document.getElementById('submitWord');
+submitWord.addEventListener('click', function (){
+    // test word
+    let userWord = new wordToFind(inputWord.value);
+    userWord.setLetter();
+    console.log(userWord);
+    if(userWord.word === theWord.word){
+        // win : say user find the good word & random on table
+        console.log("you win");
+        answer.innerHTML = "Bravo, vous avez trouvé le mot";
+    }
+    else{
+        // account
+        chance --;
+        nbr.innerHTML = chance.toString();
+        console.log("you loose")
+        answer.innerHTML = "ce n'est pas le bon mot, vous avez perdu";
+    }
+    inputWord.value = "";
+})
 
-// loose : display word & say game over     ! count test
-
-// bonus : display pendu
-
+// // bonus : display pendu
+// let imgFond = document.createElement('div');
+// imgFond.innerHTML = '<img src="pendu-3.png" alt="pendu" height="393" width="356"/>'
+// document.getElementById('frame').prepend(imgFond);
+//
